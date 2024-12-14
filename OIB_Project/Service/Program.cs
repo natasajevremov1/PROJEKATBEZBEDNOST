a2e8d55d1bfc12ddcd7db3379886d278d10c59ac
Uros <urosgrahovac002@gmail.com>
2024-12-14 17:08:08 +0100

    Done RBAC model with three roles (Admin,Client,SpecialClient) and permisions(ExchangeSessionKey,RunService,ModifyBlacklist)

--- OIB_Project/Service/Program.cs
+++ OIB_Project/Service/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Policy;
 using System.Linq;
 using System.Security.Principal;
 using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +25,15 @@ namespace Service
             ServiceHost host = new ServiceHost(typeof(ServiceManagement));
 
             host.AddServiceEndpoint(typeof(IServiceManagement), binding, address);
+
+            // podesavamo da se koristi MyAuthorizationManager umesto ugradjenog
+            host.Authorization.ServiceAuthorizationManager = new CustomAuthorizationManager();
+
+            // podesavamo custom polisu, odnosno nas objekat principala
+            host.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.Custom;
+            List<IAuthorizationPolicy> policies = new List<IAuthorizationPolicy>();
+            policies.Add(new CustomAuthorizationPolicy());
+            host.Authorization.ExternalAuthorizationPolicies = policies.AsReadOnly();
 
             host.Open();
 
